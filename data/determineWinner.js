5e2c6ab0a9e0dfb402c937fca35e16b1a1ee37c1
egeaybars123 <egeaybars1102@gmail.com>
2022-09-24 00:20:56 +0300

    add first lottery data

--- data/determineWinner.js
+++ data/determineWinner.js
@@ -1,6 +1,9 @@
 const file = require("./participants.json");
-let participants = file.participants.sort();
-let randomNumber = 423804932740204923740049127409370532409375092430974n;
+let participants = file.participants; //names are already sorted 
+
+//see README.md for contract address to verify if only one random number was generated
+let randomNumber = 85290794060654921317835833063858394501722124600352799389211813127908428858699n;
+
 
 function determineWinners(prizeNumber) {
     let winners = [];
@@ -8,13 +11,12 @@ function determineWinners(prizeNumber) {
         const winnerIndex = randomNumber % BigInt(participants.length);
         winners.push(participants[winnerIndex]);
         participants.splice(Number(winnerIndex), 1);
-        console.log(participants);
+        console.log(participants.length);
         randomNumber >>= 1n;
     }
 
     return winners;
 }
 
-console.log(participants);
 const winners = determineWinners(1); //verilecek ödül sayısı prizeNumber parametresine yazılıyor.
 console.log(winners);
